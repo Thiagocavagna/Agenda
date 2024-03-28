68ad5953f3c49953480a771dcc33a03fad2b0e51
ALUNO <ALUNO@LABFPC20>
2024-03-28 20:25:25 -0300

    Adicionar arquivos de projeto.

---
+++ Teste/Teste.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Xml;
+
+namespace AgendaProjeto.Services
+{
+    public class Teste
+    {
+        const string folderName = "AgendaProjeto";
+
+        public void CreateDataDirectory(string userName)
+        {
+            if(!string.IsNullOrEmpty(userName))
+            {
+                var path = GetDirectory(userName);
+
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        public void CreateDataFile(AddressBook request)
+        {
+            if(!string.IsNullOrEmpty(request.UserName))
+            {
+                var directory = GetDirectory(request.UserName);
+
+                if(!Directory.Exists(directory))
+                    CreateDataDirectory(request.UserName);
+
+                string jsonContent = JsonSerializer.Serialize(request, 
+                    new JsonSerializerOptions { WriteIndented = true });
+
+                File.WriteAllText(directory, jsonContent);
+                
+            }
+        }
+
+        public string GetDirectory(string userName)
+            => Path.Combine("Y", folderName, userName);
+        
+    }
+
+    public class AddressBook
+    {
+        public string UserName { get; set; }
+        public List<Contact> Contacts { get; set; }
+    }
+
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; } 
+    }
+}
